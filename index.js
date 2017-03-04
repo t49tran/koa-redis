9a68541394eb83492e74cfa922bd28ad990e3f0a
Duong Tran <duong.tran@deepend.melbourne>
2017-03-04 17:13:56 +1100

    Fix typo bug, add ReadMe.md and use bluebird for promisifying redis client

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-/**
- * TODO: separate the redis implementation from the cache middleware
- */
 // Koa redis middleware using generic pool
 import _ from 'lodash';
 import Redis from 'redis';
 import Promise from 'bluebird';
 import genericPool from 'generic-pool';
 
-bluebird.promisifyAll(Redis.RedisClient.prototype);
-bluebird.promisifyAll(Redis.Multi.prototype);
+Promise.promisifyAll(Redis.RedisClient.prototype);
+Promise.promisifyAll(Redis.Multi.prototype);
 
 var debug = require('debug')('redis');
 
@@ -23,10 +20,9 @@ const redis = function (redis_options) {
   redis_options = _.assign({}, default_options, redis_options);
 
   const redisPool = genericPool.createPool({
-    create : () => () => {
-      return Promise(function(resolve, reject) {
+    create : () => {
+      return new Promise(function(resolve, reject) {
         let client = Redis.createClient(redis_options);
-
         client.on('error', () => {
           debug('Error connecting to redis server');
           reject({'message':'Error connect to redis server'});
@@ -46,17 +42,17 @@ const redis = function (redis_options) {
     return redisPool.acquire()
       .then(redisClient => {
         ctx.redisClient = redisClient;
-        debug('Acquire one redis connection (min: %s, max: %s, poolSize: %s)', pool_options.min, pool_options.max, redisPool.size);
+        debug('Acquire one redis connection (min: %s, max: %s, poolSize: %s)', redis_options.min, redis_options.max, redisPool.size);
         return next();
       })
       .then(() => {
         redisPool.release(ctx.redisClient);
-        debug('Release one redis connection (min: %s, max: %s, poolSize: %s)', pool_options.min, pool_options.max, redisPool.size);
+        debug('Release one redis connection (min: %s, max: %s, poolSize: %s)', redis_options.min, redis_options.max, redisPool.size);
       })
       .catch(e => {
         if (ctx.redisClient) {
           redisPool.release(ctx.redisClient);
-          debug('Release one redis connection (min: %s, max: %s, poolSize: %s)', pool_options.min, pool_options.max, redisPool.size);
+          debug('Release one redis connection (min: %s, max: %s, poolSize: %s)', redis_options.min, redis_options.max, redisPool.size);
         }
         throw e;
       });
